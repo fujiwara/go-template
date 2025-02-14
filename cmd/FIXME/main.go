9e86ba8ab2c8c04246d0f3015ccabe72a1834a5c
fujiwara <fujiwara.shunichiro@gmail.com>
2025-02-15 04:51:05 +0900

    signal handling

--- cmd/FIXME/main.go
+++ cmd/FIXME/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"context"
 	"log"
+	"os/signal"
 
 	app "github.com/fujiwara/go-template"
 )
 
 func main() {
-	ctx := context.TODO()
+	ctx, stop := signal.NotifyContext(context.Background(), signals()...)
+	defer stop()
 	if err := run(ctx); err != nil {
 		log.Fatal(err)
 	}
